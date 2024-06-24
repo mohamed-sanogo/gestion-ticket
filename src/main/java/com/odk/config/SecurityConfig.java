4cfa7c81cc1d949a33f1c0789f430cd3ebeefdf7
mohamed-sanogo <euromoh68@gmail.com>
2024-06-24 20:49:31 +0000

    basic

--- src/main/java/com/odk/config/SecurityConfig.java
+++ src/main/java/com/odk/config/SecurityConfig.java
@@ -1,42 +1,24 @@
 package com.odk.config;
 
-import com.odk.GestionTicketApplication;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    public InMemoryUserDetailsManager userDetailsService(){
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("javatalent")
-                .password("javatalent")
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests((authz) -> authz
-                .antMatchers("/message")
-                //.anyRequest()
-                .authenficated()
-                )
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
                 .httpBasic();
-        return http.build();
-
     }
 }
